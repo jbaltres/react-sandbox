89edceb4122109b2eab424546f1593f080fb4b70
Juergen Baltres <juergen.baltres@gmail.com>
2019-09-16 13:38:14 +0200

    set State and show password.length

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React from "react";
 import "./App.css";
+import PasswordInput from "./components/PasswordInput";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <PasswordInput />
+    </div>
+  );
 }
 
 export default App;
