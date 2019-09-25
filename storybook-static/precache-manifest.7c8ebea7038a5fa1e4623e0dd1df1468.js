bfe37781bbe4ffec2537f161845db5068bda3c9d
Juergen Baltres <juergen.baltres@gmail.com>
2019-09-25 17:01:59 +0200

    add several changes

---
+++ storybook-static/precache-manifest.7c8ebea7038a5fa1e4623e0dd1df1468.js
@@ -0,0 +1,15 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92b7a7aae7f76e24ca04f7f9e52ced5f",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.7e8c7b463b44ba6a27a2.bundle.js"
+  },
+  {
+    "url": "runtime~main.7e8c7b463b44ba6a27a2.bundle.js"
+  },
+  {
+    "url": "vendors~main.7e8c7b463b44ba6a27a2.bundle.js"
+  }
+]);
